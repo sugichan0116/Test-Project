0d2004b93797053d80c2c9f86c8e13bd1b2327ae
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2018-11-09 17:29:50 +0900

    seiri

---
+++ Assets/data/MusicParameter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu( menuName = "MyGame/Create MusicParameter", fileName = "MusicParameter" )]
+public class MusicParameter : ScriptableObject
+{
+    [SerializeField]
+    public string m_title = "Music";
+    [SerializeField]
+    public string m_genre = "Speed Core";
+    [SerializeField]
+    public string m_composer = "nagureo";
+    [SerializeField]
+    public Vector2 m_bpm = new Vector2(120, 0);
+    [SerializeField]
+    public List<int> m_difficulty = new List<int>(){1,1,12};
+
+} // class ParameterTable
---
+++ Assets/data/test.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField]
+    private MusicParameter  m_table = null;
+}
